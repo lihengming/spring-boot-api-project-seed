57dbb1cb4485ec80a7546caf493f74ad6ee40b71
wirechen <wirechen@gmail.com>
2017-11-15 16:18:02 +0800

    cancel test code

--- src/main/java/com/company/project/configurer/WebMvcConfigurer.java
+++ src/main/java/com/company/project/configurer/WebMvcConfigurer.java
@@ -96,16 +96,14 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
     //解决跨域问题
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "HEAD", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "TRACE");
+//        registry.addMapping("/**");
     }
 
     //添加拦截器
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         //接口签名认证拦截器，该签名认证比较简单，实际项目中可以使用Json Web Token或其他更好的方式替代。
-        if ("dev".equals(env)) { //开发环境忽略签名认证
+        if (!"dev".equals(env)) { //开发环境忽略签名认证
             registry.addInterceptor(new HandlerInterceptorAdapter() {
                 @Override
                 public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
