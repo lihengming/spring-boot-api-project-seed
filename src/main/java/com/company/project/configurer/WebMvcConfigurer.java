5a814c39a83afc59bf537e7693cb9dfab83d758b
wirechen <wirechen@gmail.com>
2017-11-15 16:18:58 +0800

    cancel test code

--- src/main/java/com/company/project/configurer/WebMvcConfigurer.java
+++ src/main/java/com/company/project/configurer/WebMvcConfigurer.java
@@ -96,7 +96,7 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
     //解决跨域问题
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**");
+        //registry.addMapping("/**");
     }
 
     //添加拦截器
