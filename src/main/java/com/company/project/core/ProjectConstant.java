3caef6844fdb32532980b15fbf89f84c67d6f75a
hengming.lhm <hengming.lhm@alibaba-inc.com>
2018-05-16 19:59:02 +0800

    update > code comment

--- src/main/java/com/company/project/core/ProjectConstant.java
+++ src/main/java/com/company/project/core/ProjectConstant.java
@@ -4,13 +4,13 @@ package com.company.project.core;
  * 项目常量
  */
 public final class ProjectConstant {
-    public static final String BASE_PACKAGE = "com.company.project";//项目基础包名称，根据自己公司的项目修改
+    public static final String BASE_PACKAGE = "com.company.project";//生成代码所在的基础包名称，可根据自己公司的项目修改（注意：项目现有的包路径需要手工修改，与这个保持一致，不然会找不到类）
 
-    public static final String MODEL_PACKAGE = BASE_PACKAGE + ".model";//Model所在包
-    public static final String MAPPER_PACKAGE = BASE_PACKAGE + ".dao";//Mapper所在包
-    public static final String SERVICE_PACKAGE = BASE_PACKAGE + ".service";//Service所在包
-    public static final String SERVICE_IMPL_PACKAGE = SERVICE_PACKAGE + ".impl";//ServiceImpl所在包
-    public static final String CONTROLLER_PACKAGE = BASE_PACKAGE + ".web";//Controller所在包
+    public static final String MODEL_PACKAGE = BASE_PACKAGE + ".model";//生成的Model所在包
+    public static final String MAPPER_PACKAGE = BASE_PACKAGE + ".dao";//生成的Mapper所在包
+    public static final String SERVICE_PACKAGE = BASE_PACKAGE + ".service";//生成的Service所在包
+    public static final String SERVICE_IMPL_PACKAGE = SERVICE_PACKAGE + ".impl";//生成的ServiceImpl所在包
+    public static final String CONTROLLER_PACKAGE = BASE_PACKAGE + ".web";//生成的Controller所在包
 
     public static final String MAPPER_INTERFACE_REFERENCE = BASE_PACKAGE + ".core.Mapper";//Mapper插件基础接口的完全限定名
 }
