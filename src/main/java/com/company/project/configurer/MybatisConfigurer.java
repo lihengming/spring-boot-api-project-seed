d86363f747c8e8169c078f342fc79e39464dcaaf
张洁 <zhangjie@zcckj.com>
2018-07-30 21:04:36 +0800

    0条记录，不执行分页逻辑

--- src/main/java/com/company/project/configurer/MybatisConfigurer.java
+++ src/main/java/com/company/project/configurer/MybatisConfigurer.java
@@ -4,10 +4,8 @@ import com.github.pagehelper.PageHelper;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
@@ -40,9 +38,12 @@ public class MybatisConfigurer {
         //配置分页插件，详情请查阅官方文档
         PageHelper pageHelper = new PageHelper();
         Properties properties = new Properties();
-        properties.setProperty("pageSizeZero", "true");//分页尺寸为0时查询所有纪录不再执行分页
-        properties.setProperty("reasonable", "true");//页码<=0 查询第一页，页码>=总页数查询最后一页
-        properties.setProperty("supportMethodsArguments", "true");//支持通过 Mapper 接口参数来传递分页参数
+        //分页尺寸为0时查询所有纪录不再执行分页
+        properties.setProperty("pageSizeZero", "true");
+        //页码<=0 查询第一页，页码>=总页数查询最后一页
+        properties.setProperty("reasonable", "true");
+        //支持通过 Mapper 接口参数来传递分页参数
+        properties.setProperty("supportMethodsArguments", "true");
         pageHelper.setProperties(properties);
 
         //添加插件
@@ -51,11 +52,12 @@ public class MybatisConfigurer {
         //添加XML目录
         ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
         //如果没有此目录的话，后续初始化SqlSessionFactory会报错，这里参考MybatisProperties的代码
+        //这样纯使用注解，没有mapper目录也可以了
         org.springframework.core.io.Resource[] resources = null;
         if (!ObjectUtils.isEmpty(resources = resolveMapperLocations("classpath:mapper/**/*.xml"))) {
-            bean.setMapperLocations(resources);
+            factory.setMapperLocations(resources);
         }
-        return bean.getObject();
+        return factory.getObject();
     }
 
     @Configuration
